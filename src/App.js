6edd8bc66d8b92d071a553f6754e9af0c74f0ed6
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-05-09 21:09:09 +0530

    hello world over

--- src/App.js
+++ src/App.js
@@ -8,14 +8,39 @@ const App = ()=>{
     let b = 5
     return (
         <div>
-           <h1 className="heading">Hello World</h1>
+           {/* <h1 className="heading">Hello World</h1>
+             
            <h1 style={obj}>value of a = {a}</h1>
             <p> {a+b} </p>
-            <p>Hello how are you</p>
+            <p>Hello how are you</p> */}
 
-            <Button />
-            <Button />
-            <Button />
+            <Button 
+               buttonText="Mango"
+               buttonColor="yellow"
+            >
+                
+            </Button>
+
+            <Button 
+               buttonText="Apple"
+               buttonColor="green"
+            >
+                <h1>Hey this is Apple</h1> 
+            </Button>
+
+            <Button 
+               buttonText="Orange"
+               buttonColor="orange"
+            />
+
+            <Button
+               buttonText="Kiwi"
+               buttonColor="tomato"
+            >
+                <h1 className="heading1">Hey this is Kiwi</h1> 
+                <p>I am virat Pet name</p>
+            </Button>
+            
             
         </div>
     )
--- src/Componets/Button.js
+++ src/Componets/Button.js
@@ -1,13 +1,52 @@
 import React from "react"; 
 
 
-const Button = (props) =>{
-   // props topics
+const Button = ({buttonText, buttonColor, children}) =>{
+
+   //  props.buttonText = "Banana" // error
+   // console.log("props",obj.buttonText) // object
+   //props =  {buttonText: "Mango", buttonColor: "red"}
+   
    return(
-        <button> Apple </button>
+      <div>
+         <button style={{color: buttonColor}}> {buttonText} </button>
+         {children}
+      </div>
    )
 }
 
 
 
 export default Button;
+
+
+// return(
+//    <div style={{backgroundColor: props.buttonColor}}>
+//      <button > {props.buttonText}  </button>
+//         {props.children}
+//    </div>
+// )
+
+
+
+// let arr = [10,20,30]
+
+// let a = arr[0]
+// let b = arr[1]
+// let c = arr[2]
+
+// let [a,,c] = arr
+
+// console.log(a,c)
+
+
+let obj = {name: "Virat", age: 32, country: "India"}
+
+// let name = obj.name
+// let age = obj.age
+// let country = obj.country
+
+let {age, name,country} = obj
+
+console.log(name,age,country)
+
